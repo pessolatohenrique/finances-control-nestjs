4cda26fd67a0350c0381a313a56ace99925abb92
pessolatohenrique <pessolatohenrique@gmail.com>
2024-04-09 18:37:37 -0300

    chore: configure database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common';
 import { EarningModule } from './earnings/earning.module';
+import { ConfigModule } from '@nestjs/config';
+import { MySqlConfigService } from './config/mysql-config.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [EarningModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      useClass: MySqlConfigService,
+      inject: [MySqlConfigService],
+    }),
+    EarningModule,
+  ],
   controllers: [],
   providers: [],
 })
---
+++ src/config/mysql-config.service.ts
@@ -0,0 +1,20 @@
+import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
+import { ConfigService } from '@nestjs/config';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class MySqlConfigService implements TypeOrmOptionsFactory {
+  constructor(private configService: ConfigService) { }
+
+  createTypeOrmOptions(): TypeOrmModuleOptions {
+    return {
+      type: 'mysql',
+      host: this.configService.get<string>('DB_HOST'),
+      port: +this.configService.get<number>('DB_PORT'),
+      username: this.configService.get<string>('DB_USERNAME'),
+      password: this.configService.get<string>('DB_PASSWORD'),
+      database: this.configService.get<string>('DB_DATABASE'),
+      synchronize: true,
+    };
+  }
+}
